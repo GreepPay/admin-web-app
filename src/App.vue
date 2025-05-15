70e0aa43256d539d2bb59cf83d1dc39d854af83b
Ayomide Akinola <drayfocus@gmail.com>
2025-05-15 11:32:58 +0100

    fix: clean up

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,63 @@
 <template>
-  <!-- <AppLoader v-if="loaderSetup.show" :setup="loaderSetup" /> -->
-  <router-view />
+    <!-- <AppLoader v-if="loaderSetup.show" :setup="loaderSetup" /> -->
+    <router-view />
 </template>
 
 <script lang="ts">
-  import { defineComponent, onMounted, ref } from "vue"
-  import { useRoute, useRouter } from "vue-router"
-  import { Logic } from "@greep/logic"
-  import { AppLoader, SetFrontendLogic } from "@greep/ui-components"
+import { defineComponent, onMounted, ref } from "vue";
+import { useRoute, useRouter } from "vue-router";
+import { Logic } from "@greep/logic";
+import { AppLoader, SetFrontendLogic } from "@greep/ui-components";
 
-  export default defineComponent({
+export default defineComponent({
     name: "App",
     component: { AppLoader },
     setup() {
-      const router: any = useRouter()
-      const route = useRoute()
+        const router: any = useRouter();
+        const route = useRoute();
 
-      const loaderSetup = ref(Logic.Common.loaderSetup)
+        const loaderSetup = ref(Logic.Common.loaderSetup);
 
-      // Set routers
-      Logic.Common.SetRouter(router)
-      Logic.Common.SetRoute(route)
+        // Set routers
+        Logic.Common.SetRouter(router);
+        Logic.Common.SetRoute(route);
 
-      // Set UI frontend logic
-      SetFrontendLogic(Logic)
+        // Set UI frontend logic
+        SetFrontendLogic(Logic);
 
-      // Set app version
-      localStorage.setItem("admin_app_version", "1.00")
+        // Set app version
+        localStorage.setItem("admin_app_version", "1.00");
 
-      // Set app url
-      Logic.Common.SetApiUrl(
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
-        (import.meta as any).env.VITE_API_URL ?? "http://localhost:3000/graphql"
-      )
+        // Set app url
+        Logic.Common.SetApiUrl(
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            (import.meta as any).env.VITE_API_URL ??
+                "http://localhost:3000/graphql",
+        );
 
-      const handleMountActions = () => {
-        const currentAuthUser = Logic.Auth.AuthUser
-        // If user is authenticated
-        if (currentAuthUser) {
-          Logic.Auth.GetAuthUser()
-        } else {
-          // Logic.Common.GoToRoute("/auth/login")
-          // const isAuthPage = window.location.pathname.includes("/auth")
-          // if (!isAuthPage) Logic.Common.GoToRoute("/auth/signup")
-          // else Logic.Common.GoToRoute("/auth/login")
-        }
-      }
+        const handleMountActions = () => {
+            const currentAuthUser = Logic.Auth.AuthUser;
+            // If user is authenticated
+            if (currentAuthUser) {
+                Logic.Auth.GetAuthUser();
+                Logic.Common.GoToRoute("/dashboards");
+            } else {
+                Logic.Common.GoToRoute("/auth/login");
+                // const isAuthPage = window.location.pathname.includes("/auth")
+                // if (!isAuthPage) Logic.Common.GoToRoute("/auth/signup")
+                // else Logic.Common.GoToRoute("/auth/login")
+            }
+        };
 
-      onMounted(() => {
-        handleMountActions()
+        onMounted(() => {
+            handleMountActions();
 
-        // watchers
-        // Logic.Common.watchProperty("loaderSetup", loaderSetup)
-      })
+            // watchers
+            // Logic.Common.watchProperty("loaderSetup", loaderSetup)
+        });
 
-      return { loaderSetup }
+        return { loaderSetup };
     },
-  })
+});
 </script>
